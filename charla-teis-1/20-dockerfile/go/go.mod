f284795274c1e47d542eefff25ed6618abedbb58
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-01-28 16:32:16 +0100

    advancement

---
+++ charla-teis-1/20-dockerfile/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/jorgeteixe/charla-teis-22/charla-teis-1/20-dockerfile/go
+
+go 1.17
---
+++ charla-teis-1/20-dockerfile/go/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	println("GET /hello")
+	fmt.Fprintf(w, "<h1>Hello world!</h1>\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
