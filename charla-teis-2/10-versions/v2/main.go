0aba1cea4c9241eeca2605dbe3d71ae34d541526
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-01-31 22:12:22 +0100

    changes

--- charla-teis-2/10-versions/v2/main.go
+++ charla-teis-2/10-versions/v2/main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	println("(v2) GET /hello")
+	println("(v2) GET /")
 	fmt.Fprintf(w, "<h1>Hello world from v2!</h1>\n")
 }
 
 func main() {
 	println("Started app...")
-	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8090", nil)
 }
